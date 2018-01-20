72a6103b7f9b72e28f0ecdb58bb2db1404ab2a94
Tim DuBois <tim@neophilus.net>
2018-01-20 19:36:28 +0100

    Spherical cow example

--- README.md
+++ README.md
@@ -2,6 +2,53 @@
 
 A high volume fraction sphere packing library based on the advancing fronts algorithm outlined in Valera *et al.*, [Computational Particle Mechanics 2, 161 (2015)](https://doi.org/10.1007/s40571-015-0045-8).
 
+> Milk production at a dairy farm was low, so the farmer wrote to the local university, asking for help from academia.
+> A multidisciplinary team of professors was assembled, headed by a theoretical physicist, and two weeks of intensive on-site investigation took place.
+> The scholars then returned to the university, notebooks crammed with data, where the task of writing the report was left to the team leader.
+> Shortly thereafter the physicist returned to the farm, saying to the farmer, "I have the solution, but it works only in the case of spherical cows in a vacuum".
+
+# Usage
+
+Complete documentation can be found at ...
+
+A simple example to get you packing spheres of radii `(0.1..0.2)` into a container sphere of radius 2.
+```rust
+extern crate nalgebra;
+extern crate rand;
+extern crate spherical_cow;
+
+use spherical_cow::shapes::Sphere;
+use rand::distributions::Range;
+use nalgebra::Point3;
+
+fn main() {
+    let boundary = Sphere::new(Point3::origin(), 2.0);
+    let mut sizes = Range::new(0.1, 0.2);
+
+    let spheres = spherical_cow::pack_spheres(boundary, &mut sizes);
+
+    println!("Number of spheres: {}", spheres.len());
+}
+```
+
+More elaborate examples can be found in the [examples](examples/) directory.
+
+# Output
+
+True to its name, it is indeed possible to build a spherical cow:
+
+![spherical cow in vacuum](examples/objects/cow_output.png)
+
+You can run this example yourself from [show_in_cow](examples/show_in_cow.rs).
+
+# Example use cases
+
+The paper which this algorithm comes from gives two examples of real world use cases:
+
+1. Sphere packing a skull model to study fractures due to shocks and penetrating objects.
+2. Sphere packing a cutting tool to identify the failure / breaking points when the tool is placed under load.
+
+The reason this library was initially written was to optimise the layout of inflatable [space habitats](https://github.com/Libbum/space-habitats) which may one day be constructed on the Moon and Mars.
 
 # License
 
---
+++ examples/show_in_cow.rs
@@ -0,0 +1,100 @@
+extern crate kiss3d;
+extern crate nalgebra;
+extern crate rand;
+extern crate obj;
+extern crate spherical_cow;
+
+use kiss3d::camera::ArcBall;
+use kiss3d::window::Window;
+use kiss3d::light::Light;
+use spherical_cow::util::{trimesh_volume, ray_intersection_count};
+use spherical_cow::{PackedVolume, Container};
+use rand::distributions::Range;
+use nalgebra::{Point3, Translation3, UnitQuaternion, Vector3};
+use obj::{Obj, SimplePolygon};
+use std::path::Path;
+
+#[derive(Clone)]
+/// A simple struct that holds triangluar information obtained from an obj file.
+struct CowBox {
+    /// Triplet identfying $V_0$, $V_1$, $V_2$ vertex locations for each
+    /// triangle in the object.
+    triangles: Vec<(Point3<f32>, Point3<f32>, Point3<f32>)>,
+}
+
+impl Container for CowBox {
+    /// Checks if sphere exists inside any given cow.
+    fn contains(&self, sphere: &spherical_cow::shapes::Sphere) -> bool {
+        // We use a ray casting to identify the sphere inside our object:
+        // shoot a ray from your point and see how many sides of the object it intersects.
+        // If the number is even, your point is outside the polygon. If it's odd your, point is inside.
+        let is_odd = |x: i32| x & 1 == 1;
+        // Distance from origin to the sphere's center point plus its radius
+        let o_dist = nalgebra::distance(&Point3::origin(), &sphere.center) + sphere.radius;
+        // Unit vector in the direction of the sphere
+        let norm_dir = sphere.center.coords / nalgebra::norm(&sphere.center.coords);
+        let count = ray_intersection_count(&self.triangles, norm_dir, o_dist);
+        if is_odd(count) {
+            // Sphere is outside, thus not contained
+            false
+        } else {
+            true
+        }
+    }
+    /// Finds the volume of our emerald.
+    /// This method can be used for any given trimesh.
+    fn volume(&self) -> f32 {
+        trimesh_volume(&self.triangles)
+    }
+}
+
+
+fn main() {
+    // Setup viewing environment.
+    let eye = Point3::new(7., 7., 7.);
+    let at = Point3::origin();
+    let mut camera = ArcBall::new(eye, at);
+
+    let mut window = Window::new_with_size("Spherical Cow: Spheres in a cow", 1920, 1080);
+    window.set_light(Light::StickToCamera);
+
+    // Load an object file from disk
+    let data = Obj::<SimplePolygon>::load(&Path::new("examples/objects/cow.obj")).unwrap();
+    let points: Vec<Point3<f32>> = data.position
+        .iter()
+        .map(|pos| Point3::new(pos[0], pos[1], pos[2]))
+        .collect();
+
+    let mut triangles: Vec<(Point3<f32>, Point3<f32>, Point3<f32>)> = Vec::new();
+    for object in data.objects.iter() {
+        for group in object.groups.iter() {
+            for poly in group.polys.iter() {
+                triangles.push((points[poly[0].0], points[poly[1].0], points[poly[2].0]));
+            }
+        }
+    }
+
+    // This is our bounding trimesh of a cow.
+    let boundary = CowBox {
+        triangles: triangles,
+    };
+
+    // Pack spheres with relatively small radii to fit in the legs and horns.
+    // WARNING: This will take a while (as in 90 minutes) to generate!!!
+    let mut sizes = Range::new(0.03, 0.05);
+    let packed = PackedVolume::new(boundary, &mut sizes);
+
+    println!("Volume Fraction: {:.2}%", packed.volume_fraction() * 100.);
+
+    // Populate spheres into scene.
+    for sphere in packed.spheres.iter() {
+        let mut scene_sphere = window.add_sphere(sphere.radius);
+        scene_sphere.set_color(rand::random(), rand::random(), rand::random());
+        scene_sphere.set_local_translation(Translation3::from_vector(sphere.center.coords));
+    }
+
+    // Show result.
+    while !window.should_close() {
+        window.render_with_camera(&mut camera);
+    }
+}
