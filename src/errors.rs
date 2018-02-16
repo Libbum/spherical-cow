a9ada29dd5094d11b356fdaa4202b34fc02a4fac
Tim DuBois <tim@neophilus.net>
2018-02-16 14:45:05 +0100

    Add error display tests

--- src/errors.rs
+++ src/errors.rs
@@ -16,11 +16,11 @@ pub enum SphericalCowError {
     /// of the container is perhaps not aligned to the origin, it is scaled too small,
     /// or the spheres you're attempting to pack are too large.
     Uncontained,
-    /// We choose a random valuc from the `set_f` vector. `rand` returns an option and we pop
+    /// We choose a random value from the `set_f` vector. `rand` returns an option and we pop
     /// the value. If it's `None` this error is thrown. Due to the contstuction of the
     /// rest of the method, it's safe to say this is unreachable.
     NoneSetF,
-    /// We choose a random valuc from the `front` vector. `rand` returns an option and we pop
+    /// We choose a random value from the `front` vector. `rand` returns an option and we pop
     /// the value. If it's `None` this error is thrown. Due to the contstuction of the
     /// rest of the method, it's safe to say this is unreachable.
     NoneFront,
@@ -35,10 +35,10 @@ impl fmt::Display for SphericalCowError {
                 write!(f, "Sphere is not contained within bounding geometry.")
             }
             SphericalCowError::NoneSetF => {
-                write!(f, "Returned none when choosing value from set f")
+                write!(f, "Returned none when choosing value from set f.")
             }
             SphericalCowError::NoneFront => {
-                write!(f, "Returned none when choosing value from front")
+                write!(f, "Returned none when choosing value from front.")
             }
         }
     }
@@ -53,5 +53,64 @@ impl Error for SphericalCowError {
             SphericalCowError::NoneSetF => "none from set f",
             SphericalCowError::NoneFront => "none from front",
         }
+    }
+}
+
+#[test]
+fn error_display_negative_radius() {
+    use nalgebra::Point3;
+    use shapes::Sphere;
+
+    if let Err(err) = Sphere::new(Point3::origin(), -1.) {
+        assert_eq!(format!("{} {}", err, err.description()), format!("Supplied radius is negative. negative radius"));
+    } else {
+        panic!("Sending a negative radius to the sphere creation function yielded an Ok result");
+    }
+}
+
+#[test]
+fn error_display_negative_extent() {
+    use shapes::Cuboid;
+
+    if let Err(err) = Cuboid::new(1., 1., -1.) {
+        assert_eq!(format!("{} {}", err, err.description()), format!("A supplied half extent is negative. negative half extent"));
+    } else {
+        panic!("Sending a negative half extent to the cuboid creation function yielded an Ok result");
+    }
+}
+
+#[test]
+fn error_display_containment() {
+    use nalgebra::Point3;
+    use shapes::Sphere;
+    use init_spheres;
+
+    let container = Sphere::new(Point3::origin(), 0.1).unwrap();
+
+    if let Err(err) = init_spheres(&[10., 15., 20.], &container) {
+        assert_eq!(format!("{} {}", err, err.description()), format!("Sphere is not contained within bounding geometry. outside bounding geometry"));
+    } else {
+        panic!("Testing a sphere which is not contained in a containter yielded an Ok result");
+    }
+}
+
+#[test]
+fn error_display_empty_values() {
+    use rand::{Rng, thread_rng};
+    use shapes::Sphere;
+
+    let empty_vec: Vec<Sphere> = Vec::new();
+    let mut rng = thread_rng();
+
+    if let Err(err) = rng.choose(&empty_vec).ok_or(SphericalCowError::NoneSetF) {
+        assert_eq!(format!("{} {}", err, err.description()), format!("Returned none when choosing value from set f. none from set f"));
+    } else {
+        panic!("set f was empty when it should not have been");
+    }
+
+    if let Err(err) = rng.choose(&empty_vec).ok_or(SphericalCowError::NoneFront) {
+        assert_eq!(format!("{} {}", err, err.description()), format!("Returned none when choosing value from front. none from front"));
+    } else {
+        panic!("front was empty when it should not have been");
     }
 }
